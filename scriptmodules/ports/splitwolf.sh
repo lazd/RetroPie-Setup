3ae1aeae794396438139e98d07b9918b2e5145bf
Larry Davis <lazdnet@gmail.com>
2018-10-30 05:21:02 +0000

    Download SoD from archive.org, copy lwmp/ folder

--- scriptmodules/ports/splitwolf.sh
+++ scriptmodules/ports/splitwolf.sh
@@ -27,7 +27,7 @@ function _get_opts_splitwolf() {
     # echo 'splitwolf-wolf3d VERSION_WOLF3D_SHAREWARE=y' # shareware v1.4
     # echo 'splitwolf-wolf3d_apogee VERSION_WOLF3D_APOGEE=y' # 3d realms / apogee v1.4 full
     echo 'splitwolf-wolf3d_full VERSION_WOLF3D=y' # gt / id / activision v1.4 full
-    # echo 'splitwolf-sod VERSION_SPEAR=y' # spear of destiny
+    echo 'splitwolf-sod VERSION_SPEAR=y' # spear of destiny
     # echo 'splitwolf-spear_demo VERSION_SPEAR_DEMO=y' # spear of destiny
 }
 
@@ -36,10 +36,10 @@ function add_games_splitwolf() {
     declare -A games=(
         # ['vswap.wl1']="Splitwolf demo"
         ['vswap.wl6']="Splitwolf"
-        # ['vswap.sd1']="Splitwolf - Spear of Destiny Ep 1"
-        # ['vswap.sd2']="Splitwolf - Spear of Destiny Ep 2"
-        # ['vswap.sd3']="Splitwolf - Spear of Destiny Ep 3"
-        ['vswap.sdm']="Splitwolf - Spear of Destiny Demo"
+        ['vswap.sod']="Splitwolf - Spear of Destiny Ep 1"
+        ['vswap.sd2']="Splitwolf - Spear of Destiny Ep 2"
+        ['vswap.sd3']="Splitwolf - Spear of Destiny Ep 3"
+        # ['vswap.sdm']="Splitwolf - Spear of Destiny Demo"
     )
     local game
     local wad
@@ -61,13 +61,14 @@ function build_splitwolf() {
         make clean
         make $defs DATADIR="$romdir/ports/splitwolf/"
         mv $bin "bin/$bin"
+        cp -r lwmp bin/
         md_ret_require+=("bin/$bin")
     done < <(_get_opts_splitwolf)
 }
 
 function install_splitwolf() {
-    mkdir -p "$md_inst/share/man"
-    cp -Rv "$md_build/man6" "$md_inst/share/man/"
+    # mkdir -p "$md_inst/share/man"
+    # cp -Rv "$md_build/man6" "$md_inst/share/man/"
     md_ret_files=('bin')
 }
 
@@ -80,10 +81,15 @@ function game_data_splitwolf() {
         # Get shareware game data
         downloadAndExtract "http://maniacsvault.net/ecwolf/files/shareware/wolf3d14.zip" "$romdir/ports/splitwolf" "-j -LL"
     fi
-    if [[ ! -f "$romdir/ports/splitwolf/vswap.sdm" && ! -f "$romdir/ports/splitwolf/vswap.sod" ]]; then
+    if [[ ! -f "$romdir/ports/splitwolf/vswap.sod" ]]; then
         cd "$__tmpdir"
         # Get shareware game data
-        downloadAndExtract "http://maniacsvault.net/ecwolf/files/shareware/soddemo.zip" "$romdir/ports/splitwolf" "-j -LL"
+        downloadAndExtract "http://archive.org/download/DOS.Memories.Project.1980-2003/DOS.Memories.Project.1980-2003.zip/Spear%20Of%20Destiny%20%281992%29%28Formgen%29.zip" "$romdir/ports/splitwolf" "-j -LL"
+    fi
+    if [[ ! -f "$romdir/ports/splitwolf/vswap.sd2"  || ! -f "$romdir/ports/splitwolf/vswap.sd3" ]]; then
+        cd "$__tmpdir"
+        # Get shareware game data
+        downloadAndExtract "http://archive.org/download/DOS.Memories.Project.1980-2003/DOS.Memories.Project.1980-2003.zip/Spear%20of%20Destiny%20Mission%20Packs%20%281994%29%28FormGen%20Inc%29.zip" "$romdir/ports/splitwolf" "-j -LL"
     fi
 
     # odds n ends: http://www.veracottis.co.uk/odds.zip
@@ -121,13 +127,12 @@ function launch_splitwolf() {
         # ['6efa079414b817c97db779cecfb081c9']="splitwolf-wolf3d"
         # ['a6d901dfb455dfac96db5e4705837cdb']="splitwolf-wolf3d_apogee"
         ['b8ff4997461bafa5ef2a94c11f9de001']="splitwolf-wolf3d_full"
-        # ['b1dac0a8786c7cdbb09331a4eba00652']="splitwolf-sod --mission 1"
-        # ['25d92ac0ba012a1e9335c747eb4ab177']="splitwolf-sod --mission 2"
-        # ['94aeef7980ef640c448087f92be16d83']="splitwolf-sod --mission 3"
-        # ['35afda760bea840b547d686a930322dc']="splitwolf-spear_demo"
+        ['b1dac0a8786c7cdbb09331a4eba00652']="splitwolf-sod"
+        ['25d92ac0ba012a1e9335c747eb4ab177']="splitwolf-sod --mission 2"
+        ['94aeef7980ef640c448087f92be16d83']="splitwolf-sod --mission 3"
     )
         if [[ "\${game_checksums[\$(get_md5sum \$wad_file)]}" ]] 2>/dev/null; then
-            $md_inst/bin/\${game_checksums[\$(get_md5sum \$wad_file)]} --split=2 --splitlayout=2x1
+            $md_inst/bin/\${game_checksums[\$(get_md5sum \$wad_file)]} --split 2 --splitlayout 2x1
         else
             echo "Error: \$wad_file (md5: \$(get_md5sum \$wad_file)) is not a supported version"
         fi
